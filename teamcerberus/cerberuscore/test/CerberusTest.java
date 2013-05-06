1b40508357bbd0f54088d74f024ad16d2c594fcc
Cayde Dixon <cayde.dixon@gmail.com>
2013-05-06 13:59:48 +1000

    Added some tests
    Signed-off-by: Cayde Dixon <cayde.dixon@gmail.com>

---
+++ teamcerberus/cerberuscore/test/CerberusTest.java
@@ -0,0 +1,38 @@
+package teamcerberus.cerberuscore.test;
+
+import java.lang.reflect.Field;
+
+import net.minecraftforge.common.Configuration;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.Mod.Init;
+
+@Mod(modid="CerberusTest", version="1")
+public class CerberusTest {
+	@Instance
+	public static CerberusTest instance;	
+	public Config config;
+	
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event) {
+		config = new Config(new Configuration(event.getSuggestedConfigurationFile()));
+		for (Field f : config.getClass().getDeclaredFields())
+			try {
+				System.out.println(f.get(config));
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	
+	@Init
+	public void init(FMLInitializationEvent event) {
+		
+	}
+}
